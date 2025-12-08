81a9bccd9f65ab931c199c220e07cdfeae151c8a
nescioquid <info@creachr.com>
2025-12-08 18:00:46 -0500

    resizes and realigns A and B buttons

--- src/lib/AAndB.svelte
+++ src/lib/AAndB.svelte
@@ -5,14 +5,14 @@
 <div class="relative size-[25vh]">
   <button
     title="A"
-    class="btn btn-circle btn-success opacity-90 border-6 size-[15vh] absolute right-0 -translate-y-2/3"
+    class="btn btn-circle btn-success opacity-90 border-6 size-[12vh] absolute right-0"
   >
-    <Plus class="opacity-50 size-[10vh]" />
+    <Plus class="opacity-50 size-[9vh]" />
   </button>
   <button
     title="B"
-    class="btn btn-circle btn-error opacity-90 border-6 size-[15vh] absolute left-0 translate-y-1/3"
+    class="btn btn-circle btn-error opacity-90 border-6 size-[12vh] absolute left-0 translate-y-1/1"
   >
-    <Minus class="opacity-50 size-[10vh]" />
+    <Minus class="opacity-50 size-[9vh]" />
   </button>
 </div>
