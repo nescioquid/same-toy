42af6253791814710404040a6ff09cecf07a3fcb
nescioquid <info@creachr.com>
2025-12-08 19:01:34 -0500

    changes the default text in the Screen

--- src/lib/Screen.svelte
+++ src/lib/Screen.svelte
@@ -2,8 +2,8 @@
   class="flex items-center justify-center bg-gray-950 rounded-2xl h-[60vh] w-[90vh] border-6 border-gray-800 opacity-85"
 >
   <slot>
-    <span class="text-purple-300 font-mono text-[4vh] opacity-25">
-      INSERT GAME
+    <span class="text-gray-900 font-mono text-[6vh] opacity-85">
+      POWER ME ON PLEASE
     </span>
   </slot>
 </div>
