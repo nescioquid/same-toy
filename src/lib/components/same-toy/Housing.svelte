38a1e2d3132ec67716306f05295b50fe488a7561
nescioquid <info@creachr.com>
2025-12-09 10:07:42 -0500

    changes Housing component to look more like a handheld

--- src/lib/components/same-toy/Housing.svelte
+++ src/lib/components/same-toy/Housing.svelte
@@ -2,7 +2,7 @@
   class="card flex-row gap-[3vh] items-center justify-center border-[2vh] border-purple-800 rounded-4xl h-[80vh] w-[160vh] p-[6vh] pt-[9vh] opacity-90 bg-linear-to-br from-purple-700 to-blue-600"
 > -->
 <div
-  class="card flex-row gap-[3vh] items-center justify-center border-[2vh] border-violet-800 rounded-t-[20vh] rounded-b-[25vh] h-[80vh] w-[160vh] p-[6vh] pt-[9vh] opacity-90 bg-linear-to-t from-violet-700 to-indigo-500"
+  class="card flex-row gap-[3vh] items-center justify-center border-[2vh] border-violet-800 rounded-t-[20vh] rounded-b-[30vh] h-[80vh] w-[160vh] p-[6vh] pt-[9vh] opacity-90 bg-linear-to-t from-violet-700 to-indigo-500"
 >
   <slot />
 </div>
