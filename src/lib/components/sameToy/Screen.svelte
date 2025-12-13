f4f79ae33f51fa90c863a4493977f6ebae753a57
nescioquid <info@creachr.com>
2025-12-13 01:21:19 -0500

    changes UI to statically load Kaplay component

--- src/lib/components/sameToy/Screen.svelte
+++ src/lib/components/sameToy/Screen.svelte
@@ -1,19 +1,27 @@
 <script>
   import Kaplay from '$lib/components/Kaplay.svelte'
   import { power } from '$lib/power.svelte'
+  import { PowerOff } from '@lucide/svelte'
 </script>
 
 <div
-  class="flex items-center justify-center bg-gray-900 rounded-2xl h-[60vh] w-[96vh] border-[3vh] border-gray-700 opacity-90"
+  class="relative flex items-center justify-cente rounded-2xl h-[60vh] w-[96vh] border-[3vh] border-gray-800 shadow-xl"
 >
-  {#if power.onOff}
-    <!-- <span class="text-gray-600 font-mono text-[6vh] opacity-70">
-      HELLO WORLD
-    </span> -->
-    <Kaplay />
-  {:else}
-    <span class="text-gray-600 font-mono text-[6vh] opacity-70">
-      PLAY WITH ME
-    </span>
+  <Kaplay power={power.onOff} />
+
+  {#if !power.onOff}
+    <div
+      class="absolute inset-0 pointer-events-none bg-linear-to-t from-violet-800 to-indigo-600"
+    >
+      <div
+        class="flex items-center justify-center h-[54.1vh] w-[90.3vh] bg-linear-to-t from-gray-900 to-gray-800 opacity-90"
+      >
+        <span
+          class="items-center justify-center text-gray-200 font-mono text-[6vh] opacity-20"
+        >
+          PLAY WITH ME
+        </span>
+      </div>
+    </div>
   {/if}
 </div>
--- src/lib/power.svelte.js
+++ src/lib/power.svelte.js
@@ -3,7 +3,7 @@ export let power = $state({
 })
 
 export function togglePower() {
-  if (power.onOff) return
+  // if (power.onOff) return
   power.onOff = !power.onOff
   // console.log(`power state is: ${power.onOff}`);
 }
