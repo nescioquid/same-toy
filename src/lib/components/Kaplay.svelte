4fdf0f442967eeb6ae5ba16b9210285a4f5e9871
nescioquid <info@creachr.com>
2025-12-14 20:48:09 -0500

    loadPlayer success

--- src/lib/components/Kaplay.svelte
+++ src/lib/components/Kaplay.svelte
@@ -2,9 +2,7 @@
   import { onMount } from 'svelte'
   import { initKaplay } from '$lib/kaplay/kaplayConfig'
   import { OFFSET, SCALE_FACTOR } from '$lib/kaplay/constants'
-  // import { game } from '$lib/kaplay/game'
-  // import { createPlayer } from '$lib/kaplay/createPlayer'
-  // import {  } from "$lib/";
+  import { loadPlayer, playerAnim } from '$lib/kaplay/loadPlayer'
 
   let container
 
@@ -12,29 +10,26 @@
     const k = initKaplay(container)
     const { loadSprite, add, sprite, area, pos, z, scale, scene, go } = k
 
-    loadSprite('brendan', '/sprites/brendan.png', {
-      sliceX: 5,
-      sliceY: 4,
-    })
+    loadPlayer('brendan')
 
     scene('game', () => {
       let x = 6 * OFFSET
       let y = 3 * OFFSET
 
       const player = add([
-        sprite('brendan', { frame: 10 }),
+        sprite('brendan', { frame: playerAnim.south }),
         area(),
         pos(x, y),
         z(1),
         scale(SCALE_FACTOR),
-        // {
-        //   speed: 200,
-        //   direction: 'down',
-        //   isMoving: false,
-        //   target: vec2(0, 0), // overwritten right after
-        //   moveDir: vec2(0, 0),
-        // },
-        // 'player',
+        {
+          speed: 200,
+          direction: 'south',
+          isMoving: false,
+          // target: vec2(0, 0),
+          // moveDir: vec2(0, 0),
+        },
+        'player',
       ])
     })
 
---
+++ src/lib/kaplay/loadPlayer.js
@@ -0,0 +1,57 @@
+import { getKaplay } from '$lib/kaplay/kaplayConfig'
+import { SCALE_FACTOR, OFFSET } from '$lib/kaplay/constants'
+
+export const playerAnim = {
+  north: 0,
+  west: 5,
+  south: 10,
+  east: 15,
+  faceStart: 1,
+  faceEnd: 2,
+  goStart: 0,
+  goEnd: 4,
+}
+
+export function loadPlayer(spriteName, x, y, frame = 0) {
+  const k = getKaplay()
+  const { loadSprite, add, sprite, area, pos, z, scale, vec2 } = k
+
+  loadSprite(spriteName, `/sprites/${spriteName}.png`, {
+    sliceX: 5,
+    sliceY: 4,
+    anims: {
+      faceUp: {
+        from: playerAnim.north + playerAnim.faceStart,
+        to: playerAnim.north + playerAnim.faceEnd,
+      },
+      faceLeft: {
+        from: playerAnim.west + playerAnim.faceStart,
+        to: playerAnim.west + playerAnim.faceEnd,
+      },
+      faceDown: {
+        from: playerAnim.south + playerAnim.faceStart,
+        to: playerAnim.south + playerAnim.faceEnd,
+      },
+      faceRight: {
+        from: playerAnim.east + playerAnim.faceStart,
+        to: playerAnim.east + playerAnim.faceEnd,
+      },
+      goUp: {
+        from: playerAnim.north + playerAnim.goStart,
+        to: playerAnim.north + playerAnim.goEnd,
+      },
+      goLeft: {
+        from: playerAnim.west + playerAnim.goStart,
+        to: playerAnim.west + playerAnim.goEnd,
+      },
+      goDown: {
+        from: playerAnim.south + playerAnim.goStart,
+        to: playerAnim.south + playerAnim.goEnd,
+      },
+      goRight: {
+        from: playerAnim.east + playerAnim.goStart,
+        to: playerAnim.east + playerAnim.goEnd,
+      },
+    },
+  })
+}
