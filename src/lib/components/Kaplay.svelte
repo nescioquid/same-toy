1f5418e1b7fb9d50cd6044e1c0700062cb6ccca9
nescioquid <info@creachr.com>
2025-12-14 23:59:00 -0500

    gets JSON to not throw

--- src/lib/components/Kaplay.svelte
+++ src/lib/components/Kaplay.svelte
@@ -5,18 +5,34 @@
   import { loadPlayer, playerAnim } from '$lib/kaplay/loadPlayer'
   import { addPlayer } from '$lib/kaplay/addPlayer'
   import { attachPlayerMoves } from '$lib/kaplay/movePlayer'
+  import { addMap } from '$lib/kaplay/addMap'
 
   let container
 
   onMount(() => {
     const k = initKaplay(container)
-    const { loadSprite, add, sprite, area, pos, z, scale, scene, go, vec2 } = k
+    const { loadSprite, loadJSON, add, sprite, pos, z, scale, scene, go } = k
 
     loadPlayer('brendan')
+
+    loadSprite('bedroom-lg', '/sprites/bedroom-lg.png')
+
+    const mapData = loadJSON('bedroom-lg', `/sprites/bedroom-lg.json`)
 
     scene('game', () => {
       const player = addPlayer('brendan', 6, 3, 'south')
       attachPlayerMoves(player)
+
+      const map = add([
+        sprite('bedroom-lg'),
+        pos(0, 0),
+        z(0),
+        scale(SCALE_FACTOR),
+      ])
+
+      mapData.then((mapJSON) => {
+        addMap(mapJSON)
+      })
 
       // loadPlayer('brendan')
       //   .then(() => loadMap('bedroom'))
--- src/lib/kaplay/addMap.js
+++ src/lib/kaplay/addMap.js
@@ -1,25 +1,21 @@
 import { getKaplay } from '$lib/kaplay/kaplayConfig'
 import { SCALE_FACTOR } from '$lib/kaplay/constants'
 
-export async function createMap(mapName) {
+export function addMap(mapData) {
   const k = getKaplay()
-  const { loadSprite, loadJSON, getData, add, sprite, pos, z, scale, area, Rect, vec2, body } = k
-
-  await loadSprite(mapName, `/sprites/${mapName}.png`)
-
-  const mapData = await loadJSON(mapName, `/sprites/${mapName}.json`)
-
-  const map = add([sprite(mapName), pos(0, 0), z(0), scale(SCALE_FACTOR)])
+  const { add, area, Rect, vec2, body, pos } = k
 
   for (const layer of mapData.layers) {
     if (layer.type === 'tilelayer') continue
 
     if (layer.name === 'Colliders') {
       for (const object of layer.objects) {
-        map.add([
+        // map.add([
+        add([
           area({ shape: new Rect(vec2(0), object.width, object.height) }),
           body({ isStatic: true }),
           pos(object.x, object.y),
+          'wall',
         ])
       }
       continue
